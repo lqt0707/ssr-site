2e08705f76a6cd71bda35f63d6b876d045e294ac
liuqiutao <qiutao.liu@7x-networks.com>
2022-10-09 15:31:40 +0800

    feat: 项目初始化配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        "type-enum": [2, "always", ["feat", "fix", "revert"]],
-        "subject-max-length": [1, "always", 30],
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "revert"]],
+    "subject-max-length": [1, "always", 30],
+  },
 };
